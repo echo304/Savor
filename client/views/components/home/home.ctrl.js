cc4ba68a78172e2c5a21ef9c0a15a26adb7ec476
S.B. Lee <echo3042@gmail.com>
2016-06-10 20:21:57 -0700

    (Feat) Add google map on the home page

--- client/views/components/home/home.ctrl.js
+++ client/views/components/home/home.ctrl.js
@@ -2,12 +2,12 @@
 var underscore = angular.module('underscore', []);
 underscore.factory('_', function() {
   //Underscore must already be loaded on the page
-  return window._; 
+  return window._;
 });
 
 angular
-  .module('savor.home',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'underscore'])
-  .controller('homeController', function($scope, $http, _) {
+  .module('savor.home',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'underscore', 'uiGmapgoogle-maps'])
+  .controller('homeController', function($scope, $http, _, uiGmapGoogleMapApi) {
     //refresh function that was an attempt to get just added restaurant to render on page without a refresh
     /*window.refresh = function() {
       $http.get('/api/restaurants').then(function (response) {
@@ -16,13 +16,24 @@ angular
       })
     };*/
 
+    // mock data of google map
+    $scope.map = {
+      center: {
+        latitude: 45,
+        longitude: -73
+      },
+      zoom: 8
+    };
+
     $scope.profile = JSON.parse(localStorage.getItem('profile'));
-    
+
     function getAll() {
       $http.get('/api/restaurants').then(function(res) {
         $scope.restaurants = res.data;
       });
     }
-    getAll();
+    // Make sure to wait till Google Maps SDK is fully ready
+    uiGmapGoogleMapApi.then(function(maps) {
+      getAll();
+    });
   });
-  
